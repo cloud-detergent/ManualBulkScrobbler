034b0dba83ac2b2cae70437315d93ee2044bc77b
Andrey Petrov <andrey.petrov@rokolabs.com>
2022-08-18 13:05:49 +0400

    refactored last.fm api functions

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-// rivendellscrobbler
+// ManualBulkScrobbler
 // app.js
 
-var fs = require('fs');
-var path = require('path');
-var LastfmAPI = require('lastfmapi');
+import fs from "fs";
+import path from "path";
+import LastfmAPI from "lastfmapi";
 
 /*
-Start script checking for updates to input file,
-expecting live audio data from Rivendell of the form:
-`[artist] --- [track]`
+Start script checking input file,
+expecting audio track list in the form:
+
+`[artist] --- [track] --- [datetime]`
+
+Datetime parameter is non-required. If there is no datetime stamps in the file, the script will ask it.
+All of subsequent tracks will be assumed to be the length of 4 minutes.
 
 Will scrobble new updates to Last.fm with api data from passwords.json:
 {
@@ -19,44 +23,18 @@ Will scrobble new updates to Last.fm with api data from passwords.json:
 }
 */
 
-var passwords = require('./passwords.json');
+import passwords from "./passwords.json";
 
-var lfm = new LastfmAPI({
+let lfm = new LastfmAPI({
 	'api_key' : passwords.lastfm_api_key,
 	'secret' : passwords.lastfm_secret
 });
 
-var lastTimestamp = null;
-var storePath = path.resolve(__dirname, "store.json");
-var scrobbleHistoryLocation = path.resolve(__dirname, "scrobbleHistory.txt");
+let lastTimestamp = null;
+let storePath = path.resolve(__dirname, "store.json");
+let scrobbleHistoryLocation = path.resolve(__dirname, "scrobbleHistory.txt");
 
-/*
-Creates a Last.fm session and executes callback within that session
-*/
-function authenticateSession(callback) {
-	lfm.auth.getMobileSession(passwords.lastfm_user, passwords.lastfm_password, function(err, session) {
-		if (err) {
-			console.log(err);
-		}
-		lfm.setSessionCredentials(session.username, session.key);
-		callback();
-	});
-}
 
-/*
-Scrobble a track on Last.fm
-Should be executed within a valid session
-*/
-function scrobble(artist, track, timestamp) {
-	lfm.track.scrobble({
-		'artist' : artist,
-		'track' : track,
-		'timestamp' : timestamp
-	}, function (err, scrobbles) {
-		if (err) { return console.log('Failed to scrobble!', artist, track, ":", err); }
-		console.log('Scrobbled:', scrobbles);
-	});
-}
 
 
 /*
@@ -69,27 +47,27 @@ if (!fs.existsSync(storePath)) {
 
 // start script with file from command `node app.js [/path/to/audio.txt]`
 // read audio file every 5 seconds, scrobble if updated
-var file = process.argv[2];
+let file = process.argv[2];
 setInterval(function() {
 	console.log("checking for new audio data...");
 
-	var rivendellDataPath = path.resolve(__dirname, file);
-	fs.readFile(rivendellDataPath, 'utf8', function(err, rivendellData) {
+	let audioDataPath = path.resolve(__dirname, file);
+	fs.readFile(audioDataPath, 'utf8', (err, audioData) => {
 		if (err) {
 			console.log(err);
-		} else if (rivendellData) {
+		} else if (audioData) {
 			fs.readFile(storePath, 'utf8', function(err, storeData) {
 				if (err) {
 					console.log(err);
 				} else {
-					var store = JSON.parse(storeData);
+					let store = JSON.parse(storeData);
 
 					// read live audio data and scrobble if it's new
-					var pattern = /([^\n]*)---([^\n]*)/g;
-					var match = pattern.exec(rivendellData);
+					let pattern = /([^\n]*)---([^\n]*)/g;
+					let match = pattern.exec(audioData);
 					if (match) {
-						var artist = match[1];
-						var track = match[2];
+						let artist = match[1];
+						let track = match[2];
 						if (artist && track) {
 							artist = artist.trim();
 							track = track.trim();
@@ -99,7 +77,7 @@ setInterval(function() {
 									console.log(new Date().toLocaleString(), 'found a new track to scrobble:', artist, '---', track);
 									scrobble(artist, track, Math.floor(Date.now() / 1000));
 
-									var json = JSON.stringify({
+									let json = JSON.stringify({
 										artist: artist,
 										track: track
 									});
